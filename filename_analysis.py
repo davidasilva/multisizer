c1138a50b4a516a5256e0e8cc6215896d7c66f69
Davi da Silva <davi@uchicago.edu>
2017-06-22 13:23:09 -0400

    creating filename_analysis file
    contains functions and regex patterns for converting filenames to column
    data

---
+++ filename_analysis.py
@@ -0,0 +1,14 @@
+'''A collection of functions and regular expression patterns that are broadly useful for extracting experiment data from filenames, via the regExColum method of coulterExperiment objects.'''
+
+pmDict = {
+    '+':True,
+    '-':False}
+
+def plusMinusToBool(pm):
+    if pm not in ['+','-']: return None
+    else: return pmDict[pm]
+    
+drugPattern = '([+-])drug'
+experimentTimePattern = 't=([0-9]*\.*[0-9])[a-zA-Z]*__' #gets the float part of the time in, e.g., 't=1.4d'
+experimentTimeUnitPattern = 't=[0-9]*\.*[0-9]([a-zA-Z]*)__' #gets the time unit part of, e.g. 't=1.4d' (would return 'd')
+cellTypePattern = '[0-9]*-[0-9]*__(.*)__[+-]'
